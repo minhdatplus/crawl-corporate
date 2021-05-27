c3c3744e02bcf3021ad9013526c95537449964c4
dat_dm_vn <datdm@ascendsgroup.com>
2021-05-28 00:01:15 +0700

    update swagger

--- src/main/java/com/example/crawlssi/outgate/ssi/SsiClient.java
+++ src/main/java/com/example/crawlssi/outgate/ssi/SsiClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "ssiClient", url = "${url.host}")
+@FeignClient(name = "ssiClient", url = "${url.fundamental.host}")
 public interface SsiClient {
 
     @GetMapping("${url.endpoint.defaultallstock}")
